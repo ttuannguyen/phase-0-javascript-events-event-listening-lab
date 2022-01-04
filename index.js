f165888847711d119b196985d921774219c855c3
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-01-03 17:10:17 -1000

    commit tree 95d5888e82bc1afdbdac7f2cac875404618f9c72

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
-}
+//Create Event Listeners on DOM Nodes with addEventListener()
+// const input = document.getElementById('input');
+
+//If calling our callback function in one place
+//input.addEventListener('click', () => alert('I was clicked!'));
+
+//if using that same message on multiple elements
+// function clickAlert() {
+//     alert('I was clicked');
+// }
+// input.addEventListener('click', clickAlert);
+
+//LAB WORK:
+
+const input = document.getElementById('input');
+function addingEventListener() { 
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+})}
